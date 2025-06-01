b66c32102a73972f1917370a831c452aab056ac9
Tomoki108 <shinonome81@gmail.com>
2025-06-01 14:42:32 +0900

    summary-web schelton

---
+++ services/summary-web/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>About Summary Web</h1>
+        <p>This is the about page of the summary web application.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutView'
+}
+</script>
+
+<style scoped>
+.about {
+    padding: 20px;
+}
+</style>
---
+++ services/summary-web/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <h1>Summary Web</h1>
+        <p>This is the home page of the summary web application.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeView'
+}
+</script>
+
+<style scoped>
+.home {
+    padding: 20px;
+}
+</style>
